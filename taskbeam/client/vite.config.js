088da7611aa054bfadf67094a8d898da714e3806
NikolasRepo <lgndunt@gmail.com>
2026-07-12 07:34:21 -0500

    Taskbeam Project

---
+++ taskbeam/client/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import os from 'os'
+
+export default defineConfig({
+  plugins: [react()],
+  cacheDir: path.join(os.homedir(), '.vite', 'taskbeam'),
+  server: {
+    host: '0.0.0.0',
+  },
+})
